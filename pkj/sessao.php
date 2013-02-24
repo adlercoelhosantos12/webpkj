f39f9f0ec9c279222aed3f9b932451fcc0bf8d07
felipe <felipe@64.99.64.32>
2013-02-24 17:38:12 -0300

    prefixo

--- pkj/sessao.php
+++ pkj/sessao.php
@@ -15,6 +15,13 @@ function pkj_sessao_set($campo,$valor){
  $_SESSION[$campo] = $valor;
  error_reporting(-1);
 }
+function pkj_sessao_destroi(){
+ error_reporting(0); 
+ session_start();
+ session_unset();
+ session_destroy();  
+ error_reporting(-1);
+}
 
 function pkj_sessao_info(){
  error_reporting(0);
